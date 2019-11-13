675be126c2fe4d69da45b58a5b472a7616368feb
ilabstaff <ilabstaff@172.16.116.68>
2019-11-12 19:22:36 -0800

    finished bullble and selection sorter

--- src/_00_Sorting_Algorithms/BogoSorter.java
+++ src/_00_Sorting_Algorithms/BogoSorter.java
@@ -14,12 +14,36 @@ public class BogoSorter extends Sorter {
 	// It works by following these steps:
 	// STEP 1. Is the array in order?
 	//	if yes, finished; if no, go to step 2.
+	
+	
 	// STEP 2. Take two random elements in the array and swap them.
 	// STEP 3. Go back to step 1.
 	
 	//1. Complete the sort method using the Bogo sort algorithm. 
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
+		Random rand = new Random();			
+		}
+	}
 
+	static boolean intArraySorted(int[] array){
+
+		for (int i = 1; i < array.length; i++) {
+				if (array[i] < array[i-1]) {
+					return false;
+				} else {
+				
+				}
+					
+			}
+		
+		return true;
+	}
+	
+	public static void swapArrayOfTwo(int[] arr) {
+		 int[] arr1 = {0, 0};
+		 arr1[0] = arr[1];
+		 arr1[1] = arr[0];	
+		 System.out.println();
 	}
 }
--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -3,6 +3,7 @@ package _00_Sorting_Algorithms;
 public class BubbleSorter extends Sorter{
 	public BubbleSorter() {
 		type = "Bubble";
+	
 	}
 	
 	//1. Use the bubble sorting algorithm to sort the array.
@@ -10,7 +11,29 @@ public class BubbleSorter extends Sorter{
 	//   progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		
+		while(!_00_SortedArrayChecker.intArraySorted(array)) {
+		for (int i = 0; i < array.length-1; i++) {
+			if (array[i]>array[i+1]) {
+				int[] arr1 = {array[i], array[i+1]};
+				System.out.println(array[i]);
+				System.out.println(array[i+1]);
+				swapArrayOfTwo(arr1);
+				array[i] = arr1[1];
+				array[i+1] = arr1[0];
+				System.out.println(array[i]);
+				System.out.println(array[i+1]);
+				
+			}
+		}
+		display.updateDisplay();
+		}								
 	}
 	
-}
+	public static void swapArrayOfTwo(int[] arr) {
+		 int[] fortnite = {0, 0};
+		 fortnite[0] = arr[1];
+		 fortnite[1] = arr[0];	
+		 System.out.println();
+	}
+	
+	}
